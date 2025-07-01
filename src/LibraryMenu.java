3f6d3b2a530505634d2bc1fba51e97a01ebd40cc
ana diru <ajd6735@psu.edu>
2025-06-30 21:31:34 -0400

    adds the display menu in the LibraryMenu

--- src/LibraryMenu.java
+++ src/LibraryMenu.java
@@ -25,8 +25,52 @@ public class LibraryMenu {
     public void displayMenu() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            // TODO - missing code
+            if (scanner.nextLine().equalsIgnoreCase("exit")) {
+                break;
+            }
+            System.out.println("Please select a numerical option: ");
+            System.out.println("1 to view all books \n2 to sort books by keyword \n" +
+                    "3 sort books by author \n4 sort books by publication year \n" +
+                    "5 search for a book by its title \n6 exit the menu");
+            int option = scanner.nextInt();
+            String word;
+
+            switch (option) {
+                case 1:
+                    logger.logViewAllBooks();
+                    break;
+                case 2:
+                    word = scanner.nextLine();
+                    logger.logSort(word);
+                    SortUtil.bubbleSort(library.getBooks(), Comparator.comparing(Book::getTitle));
+                    break;
+                case 3:
+                    word = scanner.nextLine();
+                    logger.logSort(word);
+                    SortUtil.bubbleSort(library.getBooks(), Comparator.comparing(Book::getAuthor));
+                    break;
+                case 4:
+                    System.out.println("Please enter a keyword");
+                    word = scanner.nextLine();
+                    logger.logSort(word);
+                    SortUtil.bubbleSort(library.getBooks(), Comparator.comparing(Book::getPublicationYear));
+                    break;
+                case 5:
+                    word = scanner.nextLine();
+                    Book foundBook = library.searchBookByKeyword(word);
+                    if (foundBook != null) {
+                        System.out.println("Book found: " + foundBook);
+                    } else {
+                        System.out.println("Book not found.");
+                    }
+                    logger.logSort(word);
+                    break;
+                case 6:
+                    System.out.println("Exiting the program. Good buy!");
+                    break;
+                default:
+                    System.out.println("Invalid entry.");
+            }
         }
     }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,22 @@ public class Main {
         Library library = new Library();
         library.loadBooks("src/resources/data/books.txt");
         library.viewAllBooks();
-
-        SortUtil.bubbleSort(library.getBooks(), Comparator.comparing(Book::getTitle));
-        System.out.println("After sorting by title:");
-        library.viewAllBooks();
-
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print("Enter a keyword to search (title, author, or year): ");
-
-        String keyword = keyboard.nextLine();
-        Book foundBook = library.searchBookByKeyword(keyword);
-        if (foundBook != null) {
-            System.out.println("Book found: " + foundBook);
-        } else {
-            System.out.println("Book not found.");
-        }
-
+//
+//        SortUtil.bubbleSort(library.getBooks(), Comparator.comparing(Book::getTitle));
+//        System.out.println("After sorting by title:");
+//        library.viewAllBooks();
+//
+//        Scanner keyboard = new Scanner(System.in);
+//        System.out.print("Enter a keyword to search (title, author, or year): ");
+//
+//        String keyword = keyboard.nextLine();
+//        Book foundBook = library.searchBookByKeyword(keyword);
+//        if (foundBook != null) {
+//            System.out.println("Book found: " + foundBook);
+//        } else {
+//            System.out.println("Book not found.");
+//        }
+        LibraryMenu menu = new LibraryMenu(library);
+        menu.displayMenu();
     }
 }
